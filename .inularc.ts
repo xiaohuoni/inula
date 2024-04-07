2ded56c1992a568d4a7e4511fafdf2d413214095
xiaohuoni <xiaohuoni@dingtalk.com>
2024-04-07 18:18:59 +0800

    fix: docs error

--- .inularc.ts
+++ .inularc.ts
@@ -9,6 +9,7 @@ export default {
   conventionRoutes: {
     exclude: [/\/components\//],
   },
+  azure: false,
   styles: [`.py-36 { padding-bottom: 0rem !important; }`],
   // TODO: ??? 为什么 esbuild 压缩会报错？？？
   jsMinifier: 'terser',
