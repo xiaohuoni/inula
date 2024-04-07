7c8d7802618106e0d6f620758981c02d84666eb0
xiaohuoni <xiaohuoni@dingtalk.com>
2024-04-07 18:15:30 +0800

    fix: docs error

--- .inularc.ts
+++ .inularc.ts
@@ -12,7 +12,10 @@ export default {
   styles: [`.py-36 { padding-bottom: 0rem !important; }`],
   // TODO: ??? 为什么 esbuild 压缩会报错？？？
   jsMinifier: 'terser',
+  history: {
+    type: 'hash',
+  },
   // 等后续升级 gitee page pro 需要移除这些配置
-  publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/inula/' : '/',
   base: process.env.NODE_ENV === 'production' ? '/inula/' : '/',
 };
